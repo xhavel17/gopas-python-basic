8222db51b9fb5a48d671ecb120c5d789aada244b
sokie <havelka.du@gmail.com>
2024-03-28 14:20:58 +0100

    update

---
+++ day4/jupyter/city.py
@@ -0,0 +1,9 @@
+class MyCity:
+    
+    def __init__(self, p_name, p_population, p_area):
+        self.name = p_name
+        self.population = p_population
+        self_area = p_area
+    
+    def get_city_name(self):
+        print(self.name)
